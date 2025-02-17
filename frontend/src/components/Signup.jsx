b4b8d60de3e95c3fb3495128037b15fc43157655
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2025-02-17 17:52:52 +0000

    worked on signup form

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 export const Signup = () => {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <div className="flex justify-center  h-[80vh] w-[70vw] bg-orange rounded-md text-white">
-        <h1>Create Your Account</h1>
-        <form action="/Signup">
-        <input type="email" />
-        <input type="Password" />
-        </form>
+    <div className=' flex flex-col justify-center  items-center gap-5 h-[100vh]'>
+      <h1 className='text-2xl'>Create Your Account</h1>
+      <div className='flex-col items-center h-[60%] w-[60%] bg-black rounded-md text-white p-10'>
+      <form action="">
+        <div className='flex-col '>
+          <label htmlFor="email">Enter Your Email : </label>
+          <input type="email" id="email" className="border border-white rounded-md"/>
+        </div>
+      </form>
       </div>
-      
     </div>
   )
 }
